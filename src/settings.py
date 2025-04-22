49d3343e1ac812a3598503101b881e4d901cbeae
jhonatanmsc <jhonmscosta@gmail.com>
2025-04-22 17:34:46 -0300

    static dirs root

--- src/settings.py
+++ src/settings.py
@@ -138,7 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "static/",
 ]
 
 STATIC_URL = 'static/'
