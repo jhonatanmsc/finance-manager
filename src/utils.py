127248b84c5c36236ef1218343fd009cda644a79
jhonatanmsc <jhonmscosta@gmail.com>
2025-06-25 16:42:06 -0300

    br timezone

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,11 @@
-import locale
 import calendar
-
+import locale
 from datetime import datetime
 
+import pytz
 import requests
+
+br_tz = pytz.timezone("America/Sao_Paulo")
 
 
 def real_currency(value):
@@ -12,12 +14,11 @@ def real_currency(value):
     return value
 
 
-def get_last_9_months(mode='abbr'):
+def get_last_9_months(mode="abbr"):
     today = datetime.now()
     months = []
     for i in range(9):
         month = (today.month - i - 1) % 12 + 1
-        year = today.year - ((today.month - i - 1) // 12)
         if mode == "abbr":
             months.append(calendar.month_abbr[month])
         elif mode == "number":
@@ -26,19 +27,25 @@ def get_last_9_months(mode='abbr'):
 
 
 def get_ipca(dataInicial, dataFinal):
-    url = f"https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json&dataInicial={dataInicial}&dataFinal={dataFinal}"
+    url = (
+        f"https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados"
+        f"?formato=json&dataInicial={dataInicial}&dataFinal={dataFinal}"
+    )
     res = requests.get(url)
     if res.status_code != 200:
         return 0
     dados = res.json()
-    ipca_acumulado = sum(float(item['valor']) for item in dados)
+    ipca_acumulado = sum(float(item["valor"]) for item in dados)
     return ipca_acumulado
 
 
 def get_min_salary(dataInicial, dataFinal):
-    url = f"https://api.bcb.gov.br/dados/serie/bcdata.sgs.1619/dados?formato=json&dataInicial={dataInicial}&dataFinal={dataFinal}"
+    url = (
+        f"https://api.bcb.gov.br/dados/serie/bcdata.sgs.1619/dados?"
+        f"formato=json&dataInicial={dataInicial}&dataFinal={dataFinal}"
+    )
     res = requests.get(url)
     if res.status_code != 200:
         return 0
     dados = res.json()
-    return float(dados[0]['valor'])
+    return float(dados[0]["valor"])
