bf79878fc352be2b5046697a08d57b87fd4e8f38
jhonatanmsc <jhonmscosta@gmail.com>
2025-06-25 16:44:01 -0300

    feat(goals-api): logic delete added

--- goals/serializers.py
+++ goals/serializers.py
@@ -7,15 +7,26 @@ class GoalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Goal
         fields = [
-            "id", "title", "description", "value", "created_at", "updated_at", "history",
-            "target_date", "users", "master", "concluded_at", "canceled_at", "total"
+            "id",
+            "title",
+            "description",
+            "value",
+            "created_at",
+            "updated_at",
+            "history",
+            "target_date",
+            "users",
+            "master",
+            "concluded_at",
+            "canceled_at",
+            "total",
         ]
 
     def to_internal_value(self, data):
         validated_data = super().to_internal_value(data)
 
-        if 'value' in validated_data:
-            validated_data['value'] = float(validated_data['value'])
+        if "value" in validated_data:
+            validated_data["value"] = float(validated_data["value"])
 
         return validated_data
 
