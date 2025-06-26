9bb5d23d7e8a3d001fc190873371614dceea9b04
jhonatanmsc <jhonmscosta@gmail.com>
2025-06-26 17:52:11 -0300

    feat(rest-api): commentary

--- src/urls.py
+++ src/urls.py
@@ -46,7 +46,7 @@ urlpatterns = [
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    # goals
+    # custom
     path("api/", include(goals_router.urls)),
     path("api/", include(credits_router.urls)),
     path("api/", include(debts_router.urls)),
